7caea44fee86ae7f38c0c2c00f7967fd3adf52b1
Pankaj Kumar Roy <1719pankaj@gmail.com>
2023-12-23 13:30:24 +0530

    CICD test run 17 Aspirational

--- app/src/main/java/com/example/oc/data/RnN.kt
+++ app/src/main/java/com/example/oc/data/RnN.kt
@@ -3,7 +3,7 @@ package com.example.oc.data
 class RnN {
     companion object {
         var Owner: String = "MTLI"
-        var CurrentVersion: String = "1.0.02rc"
+        var CurrentVersion: String = "1.0.03rc"
         var MaalkiKaNumber: String = "9883293901"
 
         var workingHours: Double = 7.0
